08d7f6e19927f5d9cdc99f9cfc3997ec2aef7abe
reindev <renna.vittorio@gmail.com>
2026-06-18 15:32:52 +0200

    new: use sanity for blogging

--- src/content/posts/post-1.md
+++
@@ -1,9 +0,0 @@
----
-title: "my first post"
-date: "2026-03-29"
-tags: ["tag1", "tag2", "tag3"]
----
-
-This is my first post
-
-I want to try this feature of astro.build
---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+import type { PortableTextBlock } from "sanity";
+import type { SanityImageSource } from "@sanity/image-url";
+
+export interface SanityPost {
+  _id: string;
+  title: string;
+  slug: { _type: "slug"; current: string };
+  publishedAt: string;
+  tags: string[];
+  image: SanityImageSource;
+  body: PortableTextBlock[];
+}
